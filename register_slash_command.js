b4d722227a5c674267e81fa751d2470341daf20f
Alan Newingham <48245017+01000001-01001110@users.noreply.github.com>
2024-02-02 20:59:07 -0500

    Update register_slash_command.js

--- register_slash_command.js
+++ register_slash_command.js
@@ -12,31 +12,25 @@ const commands = [
     description: 'Generates an image based on input',
     options: [
       {
-        type: 3, // Type 3 is 'STRING'
+        type: 3, // STRING type for input
         name: 'input',
         description: 'The input to generate an image from',
         required: true
-      },
-      {
-        type: 4, // Type 4 is 'INTEGER'
-        name: 'number_of_images',
-        description: 'The number of images to generate',
-        required: false // Set to true if you want it to be a mandatory field
       }
     ]
   },
   {
     name: 'describe',
-    description: 'Describes an image',
+    description: 'Describes an uploaded image or an image from a URL',
     options: [
       {
-        type: 11, // Type 11 is 'ATTACHMENT'
+        type: 11, // ATTACHMENT for image upload
         name: 'image',
         description: 'Upload an image to describe',
         required: false
       },
       {
-        type: 3, // Type 3 is 'STRING'
+        type: 3, // STRING for image URL
         name: 'image_url',
         description: 'Enter an image URL to describe',
         required: false
